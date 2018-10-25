568b90f3283b11b921a0629648c8c69eead1f40e
wryse <30854300+wryse@users.noreply.github.com>
2018-10-25 17:10:41 +0900

    Add implementation for GAN

--- src/simple_nn/nn_model.py
+++ src/simple_nn/nn_model.py
@@ -6,23 +6,9 @@ np.seterr(all='raise', under='warn')
 
 # nn model
 
-class NNModel(abc.ABC):
+class NNModelBase(abc.ABC):
     
-    def step_fit(self, X, y):
-        """Learn model with given input data set.
-        
-        Args:
-            X (np.array): input data
-            y (np.array): ground truth corresponding to X
-        
-        Returns:
-            Total cost of samples of each round in running sequence (list)
-        """
-        y_predict = self.fit_forward(X)
-        self.update_model(y_predict, y)
-        return (self.calc_loss(y_predict, y)).sum()
-    
-    def fit(self, X, y, rounds, batch_size, random_shuffle=True):
+    def fit(self, X, y, epoch=1, batch_size=None, random_shuffle=True):
         """Learn and update model with given input data set for specific rounds and batch size.
         
         Args:
@@ -32,9 +18,10 @@ class NNModel(abc.ABC):
         Returns:
             Average cost of samples of each round in running sequence (list)
         """
-        avg_cost_by_round = []
+        avg_cost_by_epoch = []
         total_size = len(X)
-        for _ in range(rounds):
+        batch_size = total_size if batch_size is None else batch_size
+        for _ in range(epoch):
             cost = 0
             learning_idx = np.arange(total_size)
             if random_shuffle:
@@ -43,8 +30,8 @@ class NNModel(abc.ABC):
             for start_idx in range(0, total_size, batch_size):
                 data_idx = learning_idx[start_idx : min(start_idx+batch_size,total_size)]
                 cost += self.step_fit(X[data_idx], y[data_idx])
-            avg_cost_by_round.append(cost/total_size)
-        return avg_cost_by_round
+            avg_cost_by_epoch.append(cost/total_size)
+        return avg_cost_by_epoch
         
     def transform(self, X):
         """Transform input data X into target data using current learned model.
@@ -59,16 +46,15 @@ class NNModel(abc.ABC):
         return self.forward(X)
     
     @abc.abstractmethod
-    def fit_forward(self, X):
-        """Forward pass, Run through each layers with given input X to get predict value.
-        Support both batch and single sample.
-        Intermediate result is stored for back propagation.
+    def step_fit(self, X, y):
+        """Learn model with given input data set.
         
         Args:
             X (np.array): input data
+            y (np.array): ground truth corresponding to X
         
         Returns:
-            Result calculated by current model(all layers)
+            Total cost of samples of each round in running sequence (list)
         """
         pass
     
@@ -85,47 +71,38 @@ class NNModel(abc.ABC):
             Result calculated by current model
         """
         pass
-    
-    @abc.abstractmethod
-    def update_model(self, y_predict, y):
-        """Update parameters of each layer using errs (y_predict - y) in current step.
-        
-        Args:
-            y_predict (np.array): predict values
-            y (np.array): target data
-        """
-        pass
-    
-    @abc.abstractmethod
-    def calc_loss(self, y_predict, y):
-        """Calculate cost value of current model of predict result.
-        
-        Args:
-            y_predict (np.array): predict values
-            y (np.array): target data
-        
-        Returns:
-            Cost values of predict results by samples
-        """
-        pass
 
-
-class VNNModel(NNModel):
+class NNModel(NNModelBase):
     """Vanilla neural network."""
     
-    def __init__(self, X_size, gd_updater, cost_function):
+    def __init__(self, X_shape, gd_updater, cost_function):
         """Init function.
         
         Attributes:
-            X_size (int): number of input data
+            X_shape (int/shape): number or shape of input data
             gd_updater (GDUpdater): gradient descent updater for GD update
             cost_function (CostFunction): cost function
             model (list(NNLayer)): layers of current model
         """
-        self.X_size = X_size
+        self.X_shape = X_shape
         self.gd_updater = gd_updater
         self.cost_function = cost_function
         self.model = []
+    
+    def step_fit(self, X, y):
+        """Learn model with given input data set.
+        
+        Args:
+            X (np.array): input data
+            y (np.array): ground truth corresponding to X
+        
+        Returns:
+            Total cost of samples of each round in running sequence (list)
+        """
+        y_predict = self.fit_forward(X)
+        self.back_propagation(y_predict, y)
+        self.update_model()
+        return (self.calc_loss(y_predict, y)).sum()
     
     def add_layer(self, layer):
         """Add one layer to the model.
@@ -133,10 +110,10 @@ class VNNModel(NNModel):
         Args:
             layer (NNLayer): Layer to be added
         """
-        input_size = self.X_size
+        input_shape = self.X_shape
         if self.model:
-            input_size = self.model[-1].node_count
-        layer.setup(input_size=input_size, gd_updater=self.gd_updater)
+            input_shape = self.model[-1].node_count
+        layer.setup(input_size=input_shape, gd_updater=self.gd_updater)
         self.model.append(layer)
     
     def fit_forward(self, X):
@@ -171,16 +148,24 @@ class VNNModel(NNModel):
             cur_res = layer.forward(cur_res)
         return cur_res
     
-    def update_model(self, y_predict, y):
-        """Update parameters of each layer using errs (y_predict - y) in current step.
+    def back_propagation(self, y_predict, y, delta=None):
+        """Back propagation to calculate gradient in last step.
+        Either estimated result-ground truth pair or delta from layer underneath should be set.
+        Store the gradients in weight_grads and bias_grads for update.
         
         Args:
             y_predict (np.array): predict values
             y (np.array): target data
+            delta (np.array): delta from layer underneath
         """
-        delta = np.atleast_2d(self.cost_function.derivative(y, y_predict))
+        if delta is None:
+            delta = np.atleast_2d(self.cost_function.derivative(y, y_predict))
         for layer in reversed(self.model):
             delta = layer.back_propagation(delta)
+        return delta
+    
+    def update_model(self):
+        """Update parameters of each layer using pre-calculated values."""
         for layer in self.model:
             layer.update()
     
@@ -195,3 +180,33 @@ class VNNModel(NNModel):
             Cost values of predict results by samples
         """
         return self.cost_function.loss(y, y_predict)
+
+
+# TODO: Add comments
+# TODO: Add parameters for Z
+class GANModel(NNModel):
+    def __init__(self, gen_model, dis_model):
+        self.gen_model = gen_model
+        self.dis_model = dis_model
+        self.Z_shape = self.gen_model.X_shape
+        self.X_shape = self.dis_model.X_shape
+    
+    def step_fit(self, X, y):
+        n = len(X)
+        Z_fake = np.random.rand(n, self.Z_shape)
+        X_fake = self.transform(Z_fake)
+        
+        self.dis_model.fit(X_fake, np.zeros((n,1)))
+        self.dis_model.fit(X, y)
+        
+        Z_fake = np.random.rand(n, self.Z_shape)
+        X_fake = self.gen_model.fit_forward(Z_fake)
+        y_predict = self.dis_model.fit_forward(X_fake)
+        delta_dis = self.dis_model.back_propagation(y_predict, np.ones(y_predict.shape))
+        self.gen_model.back_propagation(y_predict=None, y=None, delta=delta_dis)
+        self.gen_model.update_model()
+        
+        return (self.dis_model.calc_loss(y_predict, np.ones(y_predict.shape))).sum()
+    
+    def forward(self, Z):
+        return self.gen_model.transform(Z)
